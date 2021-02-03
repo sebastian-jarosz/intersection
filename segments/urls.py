355fcc256d4c79f50a4eed6b7cd692681540fa05
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2021-02-03 08:00:08 +0100

    urls.py - Path for json change

--- segments/urls.py
+++ segments/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('check_intersection/', views.check_intersection, name='check_intersection'),
     path('<int:pk>/result/', views.ChartResultView.as_view(), name='result'),
-    path('<int:pk>/', views.ChartResultJSONView.as_view(), name='result_json'),
+    path('<int:pk>/json/', views.ChartResultJSONView.as_view(), name='result_json'),
 ]
